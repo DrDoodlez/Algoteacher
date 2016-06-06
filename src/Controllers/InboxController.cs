2bf809dc251f0fd525071601437cf2293f90497c
Mikhail Probichev <mikhail.probichev@valmetpartners.com>
2016-06-07 01:02:41 +0300

    Fix Server part bug

---
+++ src/Controllers/InboxController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Algoteacher.Controllers
+{
+    public class InboxController : Controller
+    {
+        //
+        // GET: /Inbox/
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+    }
+}
